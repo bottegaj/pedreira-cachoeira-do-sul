19e9238d99892f217cdf0dac0b4d4ce184d9bb0f
bottegaj <bottegaj@gmail.com>
2026-07-29 17:11:51 -0300

    Update WhatsApp number in script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function enviarWhatsApp(event) {
 
   // TROQUE PELO NÚMERO COM DDD E CÓDIGO DO PAÍS, SOMENTE NÚMEROS.
   // Exemplo: 5551999999999
-  const numeroWhatsApp = '5551999999999';
+ const numeroWhatsApp = '5551998586743';
 
   const nome = document.getElementById('nome').value.trim();
   const material = document.getElementById('material').value.trim();
